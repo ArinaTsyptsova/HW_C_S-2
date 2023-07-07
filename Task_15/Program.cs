5804cadeca1d6f940a53b891af7ffa999d3ee275
Arina <arinacypcova@gmail.com>
2023-07-07 20:07:42 +0700

    Adedd Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 6 || num == 7)
+{
+    Console.WriteLine($"{num} -> да");
+}
+else 
+{
+    Console.WriteLine($"{num} -> нет");
+}
