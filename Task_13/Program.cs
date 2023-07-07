b258c34cf0700bb319251512196e49edd1f5d610
Arina <arinacypcova@gmail.com>
2023-07-07 20:02:53 +0700

    Adedd Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+     Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    while (num > 999) 
+    {
+        num /= 10;
+    }
+Console.WriteLine($"{num} -> {num % 10}");
+}
