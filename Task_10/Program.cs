3b952f6d79a2d88f1824c86c2fd12da60d11a0d7
Arina <arinacypcova@gmail.com>
2023-07-07 19:36:26 +0700

    Adedd Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1
+// Напишите программу, которая принимает на вход трехзначное число и на выходе
+// показывает вторую цифру этого числа
+
+Console.WriteLine("Введите число от 100 до 999: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 || num >= 1000)
+{
+     Console.WriteLine("Некорретное число");
+}
+else 
+{
+    int secondDidgit  = (num / 10) % 10;
+    Console.WriteLine($"{num} -> {secondDidgit}");
+}
